b6f599da4055ff6e707bcf799169b8cc81779c1c
Ajai-3 <ajaipjayan@gmail.com>
2025-04-28 10:27:15 +0530

    Netflix completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
-import React, { useEffect, lazy, Suspense } from "react";
+import React, { lazy, Suspense } from "react";
 import { ToastContainer } from "react-toastify";
-import { Routes, Route, useNavigate } from "react-router-dom";
-const Landing = lazy(() => import("./components/Landing"));
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useUser } from "./context/UserContext";
+
 const Login = lazy(() => import("./pages/Login"));
 const Home = lazy(() => import("./pages/Home"));
 const Player = lazy(() => import("./pages/Player"));
-import { useUser } from "./context/UserContext";
 
 const App = () => {
-
   const { user } = useUser();
 
   return (
     <>
       <ToastContainer theme="dark" />
-      <Suspense
-        fallback={
-          <div className="text-white text-center mt-10">Loading...</div>
-        }
-      >
+      <Suspense fallback={<div className="text-white text-center mt-10">Loading...</div>}>
         <Routes>
-          {/* <Route path="/" element={user ? <Home /> : <Landing />} /> */}
-          <Route path="/login" element={user ? <Home /> : <Login />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/player/:id" element={<Player />} />
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/player/:id"
+            element={user ? <Player /> : <Navigate to="/login" />}
+          />
         </Routes>
       </Suspense>
     </>
